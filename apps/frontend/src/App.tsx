c1cabb85b8ebe48443846a50ee9d1c4eba8e4e63
riadbettole <riadbettole@gmail.com>
2026-04-01 16:48:42 +0100

    feat(api): bootstrap hono server

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import heroImg from "./assets/hero.png";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0)
 
 	return (
 		<>
@@ -21,10 +21,7 @@ function App() {
 						Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
 					</p>
 				</div>
-				<button
-					className="counter"
-					onClick={() => setCount((count) => count + 1)}
-				>
+				<button className="counter" onClick={() => setCount((count) => count + 1)}>
 					Count is {count}
 				</button>
 			</section>
@@ -61,16 +58,8 @@ function App() {
 					<p>Join the Vite community</p>
 					<ul>
 						<li>
-							<a
-								href="https://github.com/vitejs/vite"
-								target="_blank"
-								rel="noopener"
-							>
-								<svg
-									className="button-icon"
-									role="presentation"
-									aria-hidden="true"
-								>
+							<a href="https://github.com/vitejs/vite" target="_blank" rel="noopener">
+								<svg className="button-icon" role="presentation" aria-hidden="true">
 									<use href="/icons.svg#github-icon"></use>
 								</svg>
 								GitHub
@@ -78,11 +67,7 @@ function App() {
 						</li>
 						<li>
 							<a href="https://chat.vite.dev/" target="_blank" rel="noopener">
-								<svg
-									className="button-icon"
-									role="presentation"
-									aria-hidden="true"
-								>
+								<svg className="button-icon" role="presentation" aria-hidden="true">
 									<use href="/icons.svg#discord-icon"></use>
 								</svg>
 								Discord
@@ -90,27 +75,15 @@ function App() {
 						</li>
 						<li>
 							<a href="https://x.com/vite_js" target="_blank" rel="noopener">
-								<svg
-									className="button-icon"
-									role="presentation"
-									aria-hidden="true"
-								>
+								<svg className="button-icon" role="presentation" aria-hidden="true">
 									<use href="/icons.svg#x-icon"></use>
 								</svg>
 								X.com
 							</a>
 						</li>
 						<li>
-							<a
-								href="https://bsky.app/profile/vite.dev"
-								target="_blank"
-								rel="noopener"
-							>
-								<svg
-									className="button-icon"
-									role="presentation"
-									aria-hidden="true"
-								>
+							<a href="https://bsky.app/profile/vite.dev" target="_blank" rel="noopener">
+								<svg className="button-icon" role="presentation" aria-hidden="true">
 									<use href="/icons.svg#bluesky-icon"></use>
 								</svg>
 								Bluesky
@@ -123,7 +96,7 @@ function App() {
 			<div className="ticks"></div>
 			<section id="spacer"></section>
 		</>
-	);
+	)
 }
 
-export default App;
+export default App
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,17 @@
-"use client";
+'use client'
 
-import type { ReactNode } from "react";
+import type { ReactNode } from 'react'
 
 interface ButtonProps {
-	children: ReactNode;
-	className?: string;
-	appName: string;
+	children: ReactNode
+	className?: string
+	appName: string
 }
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
 	return (
-		<button
-			className={className}
-			onClick={() => alert(`Hello from your ${appName} app!`)}
-		>
+		<button className={className} onClick={() => alert(`Hello from your ${appName} app!`)}>
 			{children}
 		</button>
-	);
-};
+	)
+}
