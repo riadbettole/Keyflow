d0cd36c1470e1d6207e68d92cea09187423034c3
riadbettole <riadbettole@gmail.com>
2026-04-03 21:00:46 +0100

    feat(db): add project keys join table

--- packages/db/src/schema/projects.ts
+++ packages/db/src/schema/projects.ts
@@ -9,5 +9,18 @@ export const projects = pgTable('projects', {
 	updatedAt: timestamp('updated_at').defaultNow().notNull(),
 })
 
+export const projectKeys = pgTable('project_keys', {
+	id: text('id').primaryKey(),
+	projectId: text('project_id').notNull(),
+	apiKeyId: text('api_key_id').notNull(),
+	organizationId: text('organization_id').notNull(),
+	name: text('name').notNull(),
+	createdBy: text('created_by').notNull(), // userId
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+})
+
 export type Project = typeof projects.$inferSelect
 export type NewProject = typeof projects.$inferInsert
+
+export type ProjectKey = typeof projectKeys.$inferSelect
+export type NewProjectKey = typeof projectKeys.$inferInsert
