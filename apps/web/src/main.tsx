12aee055032f69c24d3e1452ce51002b7ac1af91
riadbettole <riadbettole@gmail.com>
2026-04-04 22:52:42 +0100

    feat(web): setup tanstack router with file-based routing

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,19 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router
+	}
+}
 
 // biome-ignore lint/style/noNonNullAssertion: <Required by Vite>
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>,
 )
