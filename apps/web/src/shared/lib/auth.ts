27a3e6f0b1d64d4c50fc994ad40b6a788c993339
riadbettole <riadbettole@gmail.com>
2026-04-05 00:04:59 +0100

    feat(web): add cn utility

---
+++ apps/web/src/shared/lib/auth.ts
@@ -0,0 +1,10 @@
+import { apiKeyClient } from '@better-auth/api-key/client'
+import { organizationClient } from 'better-auth/client/plugins'
+import { createAuthClient } from 'better-auth/react'
+
+export const authClient = createAuthClient({
+	baseURL: import.meta.env.VITE_API_URL,
+	plugins: [organizationClient(), apiKeyClient()],
+})
+
+export const { signIn, signOut, signUp, useSession } = authClient
